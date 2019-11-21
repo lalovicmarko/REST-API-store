9410daa80a7092bfe160b6da2215b4004c111bbb
Marko Lalovic <marko.lalovic@wm.rs>
2019-11-21 14:07:26 +0100

    Creating MongoDB and adding Mongoose

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://node-rest-shop:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-xnbpo.mongodb.net/test?retryWrites=true&w=majority', {
+	useNewUrlParser: true, useUnifiedTopology: true
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,7 +20,7 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	if (req.method === 'OPTIONS') {
 		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-		return res.status(200).json({})
+		return res.status(200).json({});
 	}
 	next();
 });
